e9a4f4c54a1b0279e3f681f2029699915822ebbd
Clayn <clayn_osmato@gmx.de>
2021-03-01 13:43:32 +0100

    Add Eventtrigger to steps
    Steps can now configure trigger events when they should be executed. Those steps are not executed when no trigger gets invoked
    The ExecutionContext allows step to trigger an event.
    The ExecutionContext provides a SharedParameterList where steps can save objects for other steps to use. The base context parameters provided by the context are immutable for steps

--- klondike/src/main/java/de/clayntech/klondike/sdk/exec/Step.java
+++ klondike/src/main/java/de/clayntech/klondike/sdk/exec/Step.java
@@ -5,19 +5,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public abstract class Step {
 
     protected final Logger LOG= LoggerFactory.getLogger(getClass());
 
     protected final List<StepParameter<?>> parameters=new ArrayList<>();
+    protected final Set<String> trigger=new HashSet<>();
 
     public Step() {
     }
 
     public abstract void execute(ExecutionContext context) throws Exception;
 
+    public Set<String> getTrigger() {
+        return trigger;
+    }
 
     public List<StepParameter<?>> getParameter() {
         return parameters;
